962e68c165afc85053d73258d6b79472493c0c97
ThomasBrue <t.bruendl@gmail.com>
2022-02-20 11:48:25 +0100

    Start with third service

--- services/producer/src/producer.js
+++ services/producer/src/producer.js
@@ -2,15 +2,34 @@ const kafka = require("kafka-node");
 
 const client = new kafka.KafkaClient({ kafkaHost: "kafka:9092" });
 const producer = new kafka.Producer(client);
-const payload = [{ topic: "Payments", messages: "Test message" }];
+
+let messageId = 0;
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 setInterval(() => {
+  const rndInt = randomIntFromInterval(1, 1500);
+
+  const payload = [
+    {
+      topic: "Payments",
+      messages: JSON.stringify({
+        messageId: `id_${messageId++}`,
+        amount: rndInt,
+      }),
+    },
+  ];
+
   producer.send(payload, function (error, result) {
-    console.log("Sending payload to Kafka");
+    console.log("----- [producer] -----------------------------");
+
+    console.log("Sending Payments-payload to Kafka");
     if (error) {
       console.log("Sending payload failed: ", error);
     } else {
       console.log("Sending payload result:", result);
     }
   });
-}, 10000);
+}, 3000);
